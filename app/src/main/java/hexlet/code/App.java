71e5bb1194449e25940402ee4f0dce0b4ce1a24f
Sergei <mre@mail.ru>
2023-07-25 18:39:35 +0800

    gradlew

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -15,10 +15,6 @@ import static io.javalin.apibuilder.ApiBuilder.get;
 
 public final class App {
 
-    private static int getPort() {
-        String port = System.getenv().getOrDefault("PORT", "3000");
-        return Integer.valueOf(port);
-    }
     public static Javalin getApp() {
         Javalin app = Javalin.create(config -> {
                     config.plugins.enableDevLogging();
--- app/src/main/java/hexlet/code/controllers/UrlsController.java
+++ app/src/main/java/hexlet/code/controllers/UrlsController.java
@@ -56,5 +56,4 @@ public class UrlsController {
 
         ctx.redirect("/urls");
     };
-    }
 }
